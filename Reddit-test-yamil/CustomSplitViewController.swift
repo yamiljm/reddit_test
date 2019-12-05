f8f3b7d06df5ce4c5e3fb043f4ac3c43fc2304b6
Yamil Jalil Maluf <yamil.jalil@despegar.com>
2019-12-05 17:21:28 -0300

    Configure split view controller to start with the table

---
+++ Reddit-test-yamil/CustomSplitViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CustomSplitViewController.swift
+//  Reddit-test-yamil
+//
+//  Created by Yamil Jalil on 05/12/2019.
+//  Copyright © 2019 Yamil Jalil. All rights reserved.
+//
+
+import UIKit
+
+class CustomSplitViewController: UISplitViewController, UISplitViewControllerDelegate  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.preferredDisplayMode = .automatic
+    }
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController: UIViewController) -> Bool {
+
+        guard let secondaryAsNavController = secondaryViewController as? PostDetailViewController else {
+            return false
+
+        }
+
+        if secondaryAsNavController.detail == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+
+}
